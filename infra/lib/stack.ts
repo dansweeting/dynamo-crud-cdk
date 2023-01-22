412ce52d5fd3d5f047fdf7d7e8f7e1b7bbf0fd15
Dan Sweeting <dansweeting@gmail.com>
2023-01-22 19:12:04 +0000

    adds lambda resource

--- infra/lib/stack.ts
+++ infra/lib/stack.ts
@@ -2,7 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import {RemovalPolicy} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import {AttributeType, BillingMode} from 'aws-cdk-lib/aws-dynamodb';
+import * as awsLambda from "aws-cdk-lib/aws-lambda";
+import * as path from 'path'
 
 export class Stack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,10 +11,19 @@ export class Stack extends cdk.Stack {
 
         const table = new dynamodb.Table(this, "dynamoDbTable", {
             tableName: 'versionedCrudItems',
-            partitionKey: { name: 'id', type: AttributeType.STRING},
-            sortKey: { name: 'version', type: AttributeType.NUMBER},
+            partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING},
+            sortKey: { name: 'version', type: dynamodb.AttributeType.NUMBER},
             removalPolicy: RemovalPolicy.DESTROY,
-            billingMode: BillingMode.PAY_PER_REQUEST,
+            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
         });
+
+        const lambdaGetResource = new awsLambda.Function(this,'lambdaGetResource',{
+            functionName: 'getResource',
+            runtime: awsLambda.Runtime.NODEJS_18_X,
+            handler: 'lambda.getResource',
+            code: awsLambda.Code.fromAsset(path.join(__dirname, '../../tsc.out/')),
+        })
+
+        table.grantReadData(lambdaGetResource)
     }
 }
