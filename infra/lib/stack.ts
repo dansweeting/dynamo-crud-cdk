6beb642ca9eec3695eeade2529ab0b29eacf21d3
Dan Sweeting <dansweeting@gmail.com>
2023-01-22 20:01:25 +0000

    adds api gateway and fetches dynamodb object

--- infra/lib/stack.ts
+++ infra/lib/stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import {RemovalPolicy} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway'
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as awsLambda from "aws-cdk-lib/aws-lambda";
 import * as path from 'path'
@@ -22,8 +23,22 @@ export class Stack extends cdk.Stack {
             runtime: awsLambda.Runtime.NODEJS_18_X,
             handler: 'lambda.getResource',
             code: awsLambda.Code.fromAsset(path.join(__dirname, '../../tsc.out/')),
+            environment: {
+                DYNAMO_TABLE_NAME: table.tableName,
+            }
         })
 
         table.grantReadData(lambdaGetResource)
+
+        const restApi = new apigateway.RestApi(this, 'apiGateway', {
+            restApiName: 'crudRestApi',
+        })
+
+        const resource = restApi.root.resourceForPath('rest-resource/{id}')
+        resource.addMethod('GET', new apigateway.LambdaIntegration(
+            lambdaGetResource,
+        ));
+
+        new cdk.CfnOutput(this, 'crudRestApiUrl', { value: restApi.url })
     }
 }
--- src/lambda.ts
+++ src/lambda.ts
@@ -1,8 +1,25 @@
 import {APIGatewayProxyEventV2, APIGatewayProxyResultV2, Handler} from 'aws-lambda'
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+
+const { DYNAMO_TABLE_NAME: TableName } = process.env
+
+const dbClient = new DynamoDBClient({})
+const documentClient = DynamoDBDocument.from(dbClient)
 
 export const getResource: Handler<APIGatewayProxyEventV2, APIGatewayProxyResultV2>
     = async(event, context) => {
     const id = event?.pathParameters?.id
+
+    const document = await documentClient.get({
+        TableName,
+        Key: {
+            id,
+            version: 0
+        }
+    })
+
+    console.log({document})
 
     return {
         statusCode: 200,
