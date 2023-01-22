b07aee48e60499af42ab0fcc2562979ec618ecb7
Dan Sweeting <dansweeting@gmail.com>
2023-01-22 23:45:06 +0000

    adds ts-standard

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,6 +1,6 @@
-import {APIGatewayProxyEventV2, APIGatewayProxyResultV2, Handler} from 'aws-lambda'
-import {DynamoDBDocument} from "@aws-sdk/lib-dynamodb";
-import {DynamoDBClient} from "@aws-sdk/client-dynamodb";
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2, Handler } from 'aws-lambda'
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
 
 const { DYNAMO_TABLE_NAME: TableName } = process.env
 
@@ -8,88 +8,87 @@ const dbClient = new DynamoDBClient({})
 const documentClient = DynamoDBDocument.from(dbClient)
 
 const getItem = async (id: string | undefined, sortKey: string) => {
-    const { Item } = await documentClient.get({
-        TableName,
-        Key: {
-            id,
-            sortKey
-        }
-    })
+  const { Item } = await documentClient.get({
+    TableName,
+    Key: {
+      id,
+      sortKey
+    }
+  })
 
-    return Item
+  return Item
 }
 
-export const getResource: Handler<APIGatewayProxyEventV2, APIGatewayProxyResultV2>
-    = async(event, context) => {
-    const id = event?.pathParameters?.id
+export const getResource: Handler<APIGatewayProxyEventV2, APIGatewayProxyResultV2> =
+    async (event, context) => {
+      const id = event?.pathParameters?.id
 
-    const retrievedItem = await getItem(id, 'latest')
+      const retrievedItem = await getItem(id, 'latest')
 
-    if (retrievedItem === undefined) {
+      if (retrievedItem === undefined) {
         return {
-            statusCode: 404,
-            body: 'no item found'
+          statusCode: 404,
+          body: 'no item found'
         }
-    }
+      }
 
-    return {
+      return {
         statusCode: 200,
         body: JSON.stringify(retrievedItem)
+      }
     }
-}
 
 const hasConflict = async (id: string | undefined, version: number): Promise<Boolean> => {
+  const [preExistingItem, latestItem = {}] = await Promise.all([
+    getItem(id, version.toString()),
+    getItem(id, 'latest')
+  ])
 
-    const [preExistingItem, latestItem = {}] = await Promise.all([
-        getItem(id, version.toString()),
-        getItem(id, 'latest')
-    ])
+  if (preExistingItem !== undefined) {
+    console.log(`PUT conflict: Detected an item clashing with id ${id} and version ${version}.`)
+    return true
+  }
 
-    if (preExistingItem !== undefined) {
-        console.log(`PUT conflict: Detected an item clashing with id ${id} and version ${version}.`)
-        return true
-    }
+  const latestVersion = latestItem.version ?? 0
+  const expectedNextVersion = Number(latestVersion) + 1
+  if (version !== expectedNextVersion) {
+    console.log(`PUT conflict: Attempted to create new version for id ${id} version ${version}, when the expected next version number is ${expectedNextVersion}.`)
+    return true
+  }
 
-    const latestVersion = latestItem['version'] ?? 0
-    const expectedNextVersion = Number(latestVersion) + 1
-    if (version !== expectedNextVersion) {
-        console.log(`PUT conflict: Attempted to create new version for id ${id} version ${version}, when the expected next version number is ${expectedNextVersion}.`)
-        return true
-    }
-
-    return false
+  return false
 }
 
-export const putResource: Handler<APIGatewayProxyEventV2, APIGatewayProxyResultV2>
-    = async(event, context) => {
-    const id = event?.pathParameters?.id
+export const putResource: Handler<APIGatewayProxyEventV2, APIGatewayProxyResultV2> =
+    async (event, context) => {
+      const id = event?.pathParameters?.id
 
-    const body = JSON.parse(event?.body || '{}')
+      const body = JSON.parse(event?.body || '{}')
 
-    const { version = 1 } = body
+      const { version = 1 } = body
 
-    if (await hasConflict(id, version)) {
+      if (await hasConflict(id, version)) {
         return {
-            statusCode: 409
+          statusCode: 409
         }
-    }
+      }
 
-    const itemUpdates = [
+      const itemUpdates = [
         { version, sortKey: version.toString() },
-        { version, sortKey: 'latest'},
-    ].map( ({ version, sortKey }) =>
-        documentClient.put({
-            TableName,
-            Item: {
-                ...body,
-                id,
-                version,
-                sortKey
-            },
+        { version, sortKey: 'latest' }
+      ].map(async ({ version, sortKey }) =>
+        await documentClient.put({
+          TableName,
+          Item: {
+            ...body,
+            id,
+            version,
+            sortKey
+          }
         })
-    )
+      )
 
-    await Promise.all(itemUpdates)
+      await Promise.all(itemUpdates)
 
-    return { statusCode: 200 }
-}
+      return { statusCode: 200 }
+    }
--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,125 +1,133 @@
 import { v4 as guid } from 'uuid'
 
 describe('CRUD API', () => {
+  const { API_BASE_URL } = process.env
+  const apiBaseUrl = `${API_BASE_URL}/rest-resource`
 
-    const { API_BASE_URL } = process.env
-    const apiBaseUrl = `${API_BASE_URL}/rest-resource`
+  it('GET nonexistent item is a 404', async () => {
+    const url = `${apiBaseUrl}/${guid()}`
 
-    it('GET nonexistent item is a 404', async () => {
-        const url = `${apiBaseUrl}/${guid()}`
+    const response = await fetch(url, { method: 'GET' })
 
-        const response = await fetch(url, { method: 'GET'})
+    expect(response.status).toBe(404)
+  })
 
-        expect(response.status).toBe(404)
+  it('PUT item is a 200', async () => {
+    const url = `${apiBaseUrl}/${guid()}`
+
+    const response = await fetch(url, { method: 'PUT', body: JSON.stringify({ foo: 'bar' }) })
+
+    expect(response.status).toBe(200)
+  })
+
+  it('GET after PUT', async () => {
+    const id = guid()
+    const url = `${apiBaseUrl}/${id}`
+
+    const resource = { foo: 'bar', child: { foo: 'child bar' } }
+    const putResponse = await fetch(url, { method: 'PUT', body: JSON.stringify(resource) })
+
+    expect(putResponse.status).toBe(200)
+
+    const getResponse = await fetch(url, { method: 'GET' })
+    expect(getResponse.status).toBe(200)
+
+    const expectedJson = {
+      id,
+      sortKey: 'latest',
+      version: 1,
+      ...resource
+    }
+
+    const actualJson = await getResponse.json()
+
+    expect(actualJson).toEqual(expectedJson)
+  })
+
+  it('PUT is valid when version is incremented', async () => {
+    const id = guid()
+    const url = `${apiBaseUrl}/${id}`
+
+    const resource = { foo: 'bar', child: { foo: 'child bar' } }
+    const putResponseOne = await fetch(url, { method: 'PUT', body: JSON.stringify(resource) })
+
+    expect(putResponseOne.status).toBe(200)
+
+    const getResponseOne = await fetch(url, { method: 'GET' })
+    expect(getResponseOne.status).toBe(200)
+
+    const versionOne = await getResponseOne.json()
+
+    const putResponseTwo = await fetch(url, {
+      method: 'PUT',
+      body: JSON.stringify({
+        ...versionOne,
+        foo: 'baz',
+        version: 2
+      })
     })
 
-    it('PUT item is a 200', async () => {
-        const url = `${apiBaseUrl}/${guid()}`
+    expect(putResponseTwo.status).toBe(200)
+    const getResponseTwo = await fetch(url, { method: 'GET' })
+    const actualResponseTwo = await getResponseTwo.json()
 
-        const response = await fetch(url, { method: 'PUT', body: JSON.stringify({ foo: 'bar'}) })
-
-        expect(response.status).toBe(200)
+    const expectedResponseTwo = expect.objectContaining({
+      version: 2,
+      foo: 'baz'
     })
 
-    it('GET after PUT', async () => {
-        const id = guid()
-        const url = `${apiBaseUrl}/${id}`
+    expect(actualResponseTwo).toEqual(expectedResponseTwo)
+  })
 
-        const resource = { foo: 'bar', child: { foo: 'child bar'}}
-        const putResponse = await fetch(url, { method: 'PUT', body: JSON.stringify(resource) })
+  it('PUT with existing version number is a 409 conflict', async () => {
+    const id = guid()
+    const url = `${apiBaseUrl}/${id}`
 
-        expect(putResponse.status).toBe(200)
+    const resource = { foo: 'bar', child: { foo: 'child bar' } }
+    const putResponseOne = await fetch(url, { method: 'PUT', body: JSON.stringify(resource) })
 
-        const getResponse = await fetch(url, { method: 'GET'})
-        expect(getResponse.status).toBe(200)
+    expect(putResponseOne.status).toBe(200)
 
-        const expectedJson = {
-            id,
-            sortKey: 'latest',
-            version: 1,
-            ...resource
-        }
+    const getResponseOne = await fetch(url, { method: 'GET' })
+    expect(getResponseOne.status).toBe(200)
 
-        const actualJson = await getResponse.json()
+    const versionOne = await getResponseOne.json()
 
-        expect(actualJson).toEqual(expectedJson);
+    const putResponseTwo = await fetch(url, {
+      method: 'PUT',
+      body: JSON.stringify({
+        ...versionOne,
+        foo: 'baz',
+        version: 1 // trying PUT a stale version is a version conflict.
+      })
     })
 
-    it('PUT is valid when version is incremented', async () => {
-        const id = guid()
-        const url = `${apiBaseUrl}/${id}`
+    expect(putResponseTwo.status).toBe(409)
+  })
 
-        const resource = { foo: 'bar', child: { foo: 'child bar'}}
-        const putResponseOne = await fetch(url, { method: 'PUT', body: JSON.stringify(resource) })
+  it('PUT with out-of-order version number is a 409 conflict', async () => {
+    const id = guid()
+    const url = `${apiBaseUrl}/${id}`
 
-        expect(putResponseOne.status).toBe(200)
+    const resource = { foo: 'bar', child: { foo: 'child bar' } }
+    const putResponseOne = await fetch(url, { method: 'PUT', body: JSON.stringify(resource) })
 
-        const getResponseOne = await fetch(url, { method: 'GET'})
-        expect(getResponseOne.status).toBe(200)
+    expect(putResponseOne.status).toBe(200)
 
-        const versionOne = await getResponseOne.json()
+    const getResponseOne = await fetch(url, { method: 'GET' })
+    expect(getResponseOne.status).toBe(200)
 
-        const putResponseTwo = await fetch(url, { method: 'PUT', body: JSON.stringify({
-            ...versionOne,
-            foo: 'baz',
-            version: 2,
-        })})
+    const versionOne = await getResponseOne.json()
 
-        expect(putResponseTwo.status).toBe(200)
-        const getResponseTwo = await fetch(url, { method: 'GET'})
-        const actualResponseTwo  = await getResponseTwo.json();
-
-        const expectedResponseTwo = expect.objectContaining({
-            version: 2,
-            foo: 'baz'
-        })
-
-        expect(actualResponseTwo).toEqual(expectedResponseTwo)
+    const putResponseTwo = await fetch(url, {
+      method: 'PUT',
+      body: JSON.stringify({
+        ...versionOne,
+        foo: 'baz',
+        version: 99 // since the expected next version is 2
+      })
     })
 
-    it('PUT with existing version number is a 409 conflict', async () => {
-        const id = guid()
-        const url = `${apiBaseUrl}/${id}`
-
-        const resource = { foo: 'bar', child: { foo: 'child bar'}}
-        const putResponseOne = await fetch(url, { method: 'PUT', body: JSON.stringify(resource) })
-
-        expect(putResponseOne.status).toBe(200)
-
-        const getResponseOne = await fetch(url, { method: 'GET'})
-        expect(getResponseOne.status).toBe(200)
-
-        const versionOne = await getResponseOne.json()
-
-        const putResponseTwo = await fetch(url, { method: 'PUT', body: JSON.stringify({
-                ...versionOne,
-                foo: 'baz',
-                version: 1, // trying PUT a stale version is a version conflict.
-            })})
-
-        expect(putResponseTwo.status).toBe(409)
-    })
-
-    it('PUT with out-of-order version number is a 409 conflict', async () => {
-        const id = guid()
-        const url = `${apiBaseUrl}/${id}`
-
-        const resource = { foo: 'bar', child: { foo: 'child bar'}}
-        const putResponseOne = await fetch(url, { method: 'PUT', body: JSON.stringify(resource) })
-
-        expect(putResponseOne.status).toBe(200)
-
-        const getResponseOne = await fetch(url, { method: 'GET'})
-        expect(getResponseOne.status).toBe(200)
-
-        const versionOne = await getResponseOne.json()
-
-        const putResponseTwo = await fetch(url, { method: 'PUT', body: JSON.stringify({
-                ...versionOne,
-                foo: 'baz',
-                version: 99, // since the expected next version is 2
-            })})
-
-        expect(putResponseTwo.status).toBe(409)
-    })
+    expect(putResponseTwo.status).toBe(409)
+  })
 })
